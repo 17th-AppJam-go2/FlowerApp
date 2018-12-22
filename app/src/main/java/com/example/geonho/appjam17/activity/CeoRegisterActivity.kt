d873281ebba7a4beec9e87555f05cc7fa7a3fe2a
GeoN5 <sgepyh2916@naver.com>
2018-12-22 19:53:43 +0900

    ready for pull login

--- app/src/main/java/com/example/geonho/appjam17/activity/CeoRegisterActivity.kt
+++ app/src/main/java/com/example/geonho/appjam17/activity/CeoRegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.example.geonho.appjam17.activity
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.geonho.appjam17.R
+import com.example.geonho.appjam17.util.StatusBarUtil
 
 class CeoRegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        StatusBarUtil.setStatusBarColoc(window, Color.WHITE)
         setContentView(R.layout.activity_ceo_register)
     }
 }
--- app/src/main/java/com/example/geonho/appjam17/activity/RegisterChoiceActivity.kt
+++ app/src/main/java/com/example/geonho/appjam17/activity/RegisterChoiceActivity.kt
@@ -1,10 +1,12 @@
 package com.example.geonho.appjam17.activity
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.geonho.appjam17.R
 import com.example.geonho.appjam17.util.StatusBarUtil
+import kotlinx.android.synthetic.main.activity_choice_register.*
 
 class RegisterChoiceActivity : AppCompatActivity() {
 
@@ -12,5 +14,13 @@ class RegisterChoiceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         StatusBarUtil.setStatusBarColoc(window, Color.WHITE)
         setContentView(R.layout.activity_choice_register)
+
+        userButton.setOnClickListener {
+            startActivity(Intent(this,UserRegisterActivity::class.java))
+        }
+
+        ceoButton.setOnClickListener {
+            startActivity(Intent(this,CeoRegisterActivity::class.java))
+        }
     }
 }
