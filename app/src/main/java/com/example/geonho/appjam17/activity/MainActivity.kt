eaa4f6ee4c62ce4ed7d316de3140aa721abea246
GeoN5 <sgepyh2916@naver.com>
2018-12-22 16:11:43 +0900

    유틸세팅 및 패키지 구분

--- app/src/main/java/com/example/geonho/appjam17/activity/MainActivity.kt
+++ app/src/main/java/com/example/geonho/appjam17/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.geonho.appjam17
+package com.example.geonho.appjam17.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.geonho.appjam17.R
 
 class MainActivity : AppCompatActivity() {
 
