c1482f57498b7288a58d88c04eb2691e916005b2
GeoN5 <sgepyh2916@naver.com>
2018-12-22 19:50:45 +0900

    login.xml pull

---
+++ app/src/main/java/com/example/geonho/appjam17/activity/CeoRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.geonho.appjam17.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CeoRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ceo_register)
+    }
+}
